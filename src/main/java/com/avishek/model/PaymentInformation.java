b1c3ae486786d7dd938c817ff868500fbf35554f
Avishek Verma <avishekverma298@gmail.com>
2024-03-03 08:39:08 +0530

    Created models

---
+++ src/main/java/com/avishek/model/PaymentInformation.java
@@ -0,0 +1,67 @@
+package com.avishek.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+public class PaymentInformation {
+
+	@Column(name="cardholder_name")
+	private String cardHolderName;
+	
+	@Column(name="card_number")
+	private String cardNumber;
+	
+	@Column(name="expiration_date")
+	private LocalDate expirationDate;
+	
+	private String cvv;
+
+	public PaymentInformation() {
+		super();
+	}
+
+	public PaymentInformation(String cardHolderName, String cardNumber, LocalDate expirationDate, String cvv) {
+		super();
+		this.cardHolderName = cardHolderName;
+		this.cardNumber = cardNumber;
+		this.expirationDate = expirationDate;
+		this.cvv = cvv;
+	}
+
+	public String getCardHolderName() {
+		return cardHolderName;
+	}
+
+	public void setCardHolderName(String cardHolderName) {
+		this.cardHolderName = cardHolderName;
+	}
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public LocalDate getExpirationDate() {
+		return expirationDate;
+	}
+
+	public void setExpirationDate(LocalDate expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+
+	public String getCvv() {
+		return cvv;
+	}
+
+	public void setCvv(String cvv) {
+		this.cvv = cvv;
+	}
+	
+	
+}
---
+++ src/main/java/com/avishek/model/Size.java
@@ -0,0 +1,35 @@
+package com.avishek.model;
+
+public class Size {
+
+	private String name;
+	private int quantiy;
+	
+	
+	public Size() {
+		super();
+	}
+	
+	public Size(String name, int quantiy) {
+		super();
+		this.name = name;
+		this.quantiy = quantiy;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantiy() {
+		return quantiy;
+	}
+	public void setQuantiy(int quantiy) {
+		this.quantiy = quantiy;
+	}
+	
+	
+}
